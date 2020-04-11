bf61619c6efc9673a3ae9ec44426e4fad9c71f21
Sercan Dogan <sercandogan@yandex.com>
2020-04-11 20:45:36 +0300

    Add documentation and build with poetry

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # eigpca
-PCA via eigen-decomposition of the covariance/correlation matrix
+PCA via eigen-decomposition of the covariance/correlation matrix.
+
+# Install
+```bash
+pip install eigpca
+```
+
+# Example
+```python
+from eigpca import PCA
+from sklearn.datasets import load_iris
+
+X = load_iris().data
+pca = PCA()
+
+pca.fit(X)
+pca.transform(X, n_components=2)
+```
+### Scree Plot
+```python
+pca.plot(y="pov")
+```
+![Scree plot](examples/scree_plot_iris.png)
+
--- eigpca/__init__.py
+++ eigpca/__init__.py
@@ -3,14 +3,79 @@ import matplotlib.pyplot as plt
 
 
 class PCA(object):
+    """
+    Principal component analysis (PCA)
+    PCA is defined as an orthogonal linear transformation that transforms
+    the data to a new coordinate system which ıs used for feature reduction.
+
+    It provides via eigen-decomposition of the covariance/correlation matrix
+    on the contrary of sckit-learn which does via SVD.
+
+    Parameters:
+    ----------
+    corr: bool, default: True
+        if True, the eigen-decomposition will be calculated
+        using correlation matrix.
+
+    Attributes:
+    ----------
+    p_: int
+        Number of variables
+    n_: int
+        Number of observations
+    eig_vals: array
+        Eigen values of the matrix
+    eig_vecs: matrix
+        Eigen vector-matrix of the matrix
+    explained_variance_ratio_: array
+        The amount of variance explained by each of components
+
+    Examples:
+    ---------
+        from eigpca import PCA
+        X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+        pca = PCA()
+        pca.fit(X)
+        pca.transform(X, n_components=1)
+    """
+
     def __init__(self, corr=True):
+
         self.corr = corr
 
     def fit_transform(self, X, n_components=None):
+        """
+            Fit and apply dimensionality reduction
+        """
         self.fit(X)
         return self.transform(X, n_components)
 
     def transform(self, X, n_components=None):
+        """
+        Apply dimensionality reduction
+
+        Parameters
+        ----------
+        X: array-like, shape (n_observations(n), n_variables(p))
+            The data
+        n_components: int, default: None
+            Number of components
+            If it's None, then it will be set number of eigen values that are greater than 1.
+            Reason: Those with eigenvalues less than 1.00 are not considered to be stable.
+            They account for less variability than does a single variable and are not retained in the analysis.
+            In this sense, you end up with fewer factors than original number of variables. (Girden, 2001)
+            reference: https://stats.stackexchange.com/a/80318
+
+        Returns
+        -------
+        X_new: array-like, shape (n_observations, n_components)
+            Transformed values
+
+
+
+        """
+        X = self._check_shape(X)
+
         if X.shape[1] != self.p_:
             raise ValueError(f"X must have {self.p_} variables. ")
         if n_components and (n_components > self.p_ or n_components < 1):
@@ -21,6 +86,15 @@ class PCA(object):
             return np.dot(X, self.eig_vecs[:, :n_components])
 
     def fit(self, X):
+        """
+            Eigen-decomposition of X
+
+            Parameters
+            ----------
+            X: array-like, shape (n_observations(n), n_variables(p))
+                The data
+
+        """
         X = self._check_shape(X)
         self.p_ = X.shape[1]  # variables
         self.n_ = X.shape[0]  # observations
@@ -43,6 +117,9 @@ class PCA(object):
         self.explained_variance_ratio_ = self.eig_vals / self.p_
 
     def _check_shape(self, X):
+        """
+        Check shape of data
+        """
         X = np.array(X)
         if X.ndim != 2:
             raise ValueError("X must be 2 dimensional")
@@ -56,6 +133,16 @@ class PCA(object):
         return X
 
     def plot(self, y="eig"):
+        """
+        Scree plot for deciding number of components
+        Parameters
+        ----------
+        y: str, eig or pov
+            y axis
+                eig: Eigen value
+                pov: Propotion of variance
+
+        """
         if y == "eig":
             plt.plot(range(1, self.p_ + 1), self.eig_vals, 'o-')
             plt.xticks(np.arange(1, self.p_ + 1, step=1))
